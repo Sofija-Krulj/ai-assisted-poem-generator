17779181711c1d276f43800979d96aa28827ffe2
Sofija-Krulj <sofija.krulj@gmail.com>
2025-06-13 12:41:01 +0200

    Including the second function
    There seams to be a problem with the typwriter plugin

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-function fetchPoemm(event) {
-  event.preventDefault;
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+function fetchPoem(event) {
+  event.preventDefault();
 
   let apiKey = "o3cf1124f7350046738b1c34ad3dt312";
-
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  let poemnInput = document.querySelector("#form-input-poem");
+  let poemInput = document.querySelector("#form-input-poem");
   let context =
     "You are a poets Ai assitent. You helped him write the most in-depth poems that even inspire other poets.";
-  let prompt = `Plese deliver a poem according to the users ${poemInput}. Be concise, don't write too long poems.`;
-
+  let prompt = `Please generate  a poem according to the users ${poemInput.value}. Be concise, don't write too long poems.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayPoem);
 }
 
